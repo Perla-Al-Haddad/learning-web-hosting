904e1399907fccb36c5a88f7ddf4adea02795cfe
Perla-Al-Haddad <89902296+Perla-Al-Haddad@users.noreply.github.com>
2022-03-12 16:42:36 +0200

    added location based weather data for home page

---
+++ assets/js/weatherJS/script.js
@@ -0,0 +1,97 @@
+$('document').ready(function(){
+  getLocation();
+});
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    console.log("The Browser Does not Support Geolocation");
+  }
+}
+
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=metric&appid=b533307f7499889502d396e6615a0332', function(data) {
+    // let jsonResponse = document.getElementById('jsonResponse');
+    // jsonResponse.innerText = JSON.stringify(data['weather'][0]) + '\n';
+    // jsonResponse.innerText += JSON.stringify(data['main']);
+
+    let weather_data = data['weather'][0];
+    let temp_data = data['main'];
+
+    $("#mainTemp").html(Math.round(temp_data['temp']) + '°C');
+    $("#minTemp").html("Min: " + Math.round(temp_data['temp_min']) + '°C');
+    $("#maxTemp").html("Max: " + Math.round(temp_data['temp_max']) + '°C');
+    $("#weatherIcon").addClass(convertIconCode(weather_data['icon']));
+    $("#weatherDesc").html(weather_data['description']);
+    console.log(data)
+  })
+}
+
+function convertIconCode(code) {
+  switch(code) {
+    case '01d':
+    case '01n':
+      return "fa-solid fa-sun";
+    case '02d':
+    case '02n':
+      return "fa-regular fa-cloud-sun";
+    case '03d':
+    case '03n':
+      return "fa-solid fa-cloud";
+    case '04d':
+    case '04n':
+      return "fa-solid fa-cloud";
+    case '09d':
+    case '09n':
+      return "fa-solid fa-cloud-showers-heavy";
+    case '10d':
+    case '10n':
+      return "fa-solid fa-cloud-sun-rain";
+    case '11d':
+    case '11n':
+      return "fa-solid fa-bolt";
+    case '13d':
+    case '13n':
+      return "fa-solid fa-snowflake";
+    case '50d':
+    case '50n':
+      return "fa-solid fa-smog";
+  }
+}
+
+function showError(error) {
+  if(error.PERMISSION_DENIED){
+      console.log("The User have denied the request for Geolocation.");
+  }
+}
+
+function getCoords(elem) {
+  var box = elem.getBoundingClientRect();
+
+  var body = document.body;
+  var docEl = document.documentElement;
+
+  var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+  var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+  var clientTop = docEl.clientTop || body.clientTop || 0;
+  var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+  var top  = box.top +  scrollTop - clientTop;
+  var left = box.left + scrollLeft - clientLeft;
+
+  return { top: Math.round(top), left: Math.round(left) };
+}
+
+window.onscroll = function (e) {
+  let navigationCoords = getCoords(document.getElementById("mainNavigation"));
+  let headerDOM = document.getElementsByTagName("header");
+  if (headerDOM[0].offsetHeight < navigationCoords.top) {
+    $("#navigationPageIcon").removeClass("opacity-0");
+  } else {
+    $("#navigationPageIcon").addClass("opacity-0");
+  }
+}
